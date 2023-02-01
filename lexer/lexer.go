a8813a812e99c197810ead06ee28884a083b5b5a
yingsw <805962304@qq.com>
2023-02-01 21:31:07 +0800

    1.3

---
+++ lexer/lexer.go
@@ -0,0 +1,97 @@
+package lexer
+
+import "go-interpreter/token"
+
+type Lexer struct {
+	input        string
+	position     int  // 所输入字符串中的当前位置（指向当前字符）
+	readPosition int  // 所输入字符串中的当前读取位置（指向当前字符之后的一个字符）
+	ch           byte // 当前正在查看的字符
+}
+
+func NewLexer(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+}
+
+func (l *Lexer) NextToken() (tok *token.Token) {
+	tok = &token.Token{}
+
+	l.skipWhitespace()
+
+	switch s := string(l.ch); l.ch {
+	case '=':
+		tok = token.NewToken(token.ASSIGN, s)
+	case ';':
+		tok = token.NewToken(token.SEMICOLON, s)
+	case '(':
+		tok = token.NewToken(token.LPAREN, s)
+	case ')':
+		tok = token.NewToken(token.RPAREN, s)
+	case ',':
+		tok = token.NewToken(token.COMMA, s)
+	case '+':
+		tok = token.NewToken(token.PLUS, s)
+	case '{':
+		tok = token.NewToken(token.LBRACE, s)
+	case '}':
+		tok = token.NewToken(token.RBRACE, s)
+	case 0:
+		tok = token.NewToken(token.EOF, "")
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return
+		} else if isDigit(l.ch) {
+			tok = token.NewToken(token.INT, l.readNumber())
+			return
+		} else {
+			tok = token.NewToken(token.ILLEGAL, s)
+		}
+	}
+
+	l.readChar()
+	return
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func (l *Lexer) readIdentifier() string {
+	p := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[p:l.position]
+}
+
+func (l *Lexer) readNumber() string {
+	p := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[p:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z' || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+	return ch >= '0' && ch <= '9'
+}
---
+++ token/token.go
@@ -0,0 +1,53 @@
+package token
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// 标识符+字面量
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// 运算符
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// 分隔符
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// 关键字
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+type TokenType string
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+func NewToken(tp TokenType, s string) *Token {
+	return &Token{
+		Type:    tp,
+		Literal: s,
+	}
+}
